95693986d0cfc2a09c6aa76d54fe5006e7de7c70
Alberto Caparrós Trigo <acaparros87@gmail.com>
2020-10-20 19:57:49 +0200

    #2 saving selected in localstorage. Commit from clone

--- src/pods/home/home.container.tsx
+++ src/pods/home/home.container.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { HomeComponent } from './home.component';
 import { loadFullVerbCollection } from './home.api.vm';
 import { globalVerbsContext } from 'core/verbs';
+import { initialSelected } from './home.storage';
 
 export const HomeContainer = () => {
   const verbsContext = React.useContext(globalVerbsContext);
@@ -10,110 +11,15 @@ export const HomeContainer = () => {
     const fullVerbCollection = await loadFullVerbCollection();
     verbsContext.setVerbCollection(fullVerbCollection);
 
-    // TODO: store this in local storage
-    verbsContext.setSelectedVerbs([
-      'be',
-      'bear',
-      'beat',
-      'become',
-      'begin',
-      'bite',
-      'blow',
-      'break',
-      'bring',
-      'build',
-      'burn',
-      'burst',
-      'buy',
-      'can',
-      'catch',
-      'choose',
-      'come',
-      'cost',
-      'cut',
-      'deal',
-      'dig',
-      'do',
-      'draw',
-      'dream',
-      'drink',
-      'drive',
-      'eat',
-      'fall',
-      'feed',
-      'fight',
-      'find',
-      'fly',
-      'forbid',
-      'forget',
-      'forgive',
-      'freeze',
-      'get',
-      'give',
-      'go',
-      'grow',
-      'hang',
-      'have',
-      'hear',
-      'hide',
-      'hit',
-      'hold',
-      'hurt',
-      'keep',
-      'know',
-      'lead',
-      'learn',
-      'leave',
-      'lend',
-      'let',
-      'light',
-      'lose',
-      'make',
-      'mean',
-      'meet',
-      'pay',
-      'put',
-      'read',
-      'ride',
-      'ring',
-      'rise',
-      'run',
-      'say',
-      'see',
-      'sell',
-      'send',
-      'set',
-      'sew',
-      'shake',
-      'shine',
-      'shoot',
-      'show',
-      'shut',
-      'sing',
-      'sit',
-      'sleep',
-      'smell',
-      'speak',
-      'spend',
-      'stand',
-      'steal',
-      'stick',
-      'sting',
-      'swear',
-      'sweep',
-      'swim',
-      'take',
-      'teach',
-      'tear',
-      'tell',
-      'think',
-      'throw',
-      'understand',
-      'wake',
-      'wear',
-      'win',
-      'write',
-    ]);
+    const selectedVerbs: string[] = JSON.parse(
+      localStorage.getItem('selectedVerbs')
+    );
+
+    if (selectedVerbs) verbsContext.setSelectedVerbs(selectedVerbs);
+    else {
+      verbsContext.setSelectedVerbs(initialSelected);
+      localStorage.setItem('selectedVerbs', JSON.stringify(initialSelected));
+    }
   };
 
   // Let's load the full list of verbs
