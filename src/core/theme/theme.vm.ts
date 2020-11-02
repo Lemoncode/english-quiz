2d2e70a8c024736ed50e81539eb2e81bf4c7e35d
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-11-02 19:36:46 +0100

    add new theme variant with the new colors (wip)

--- src/core/theme/theme.vm.ts
+++ src/core/theme/theme.vm.ts
@@ -5,8 +5,15 @@ import {
 } from '@material-ui/core/styles/createPalette';
 
 interface Palette extends DefaultPalette {
-  table: {
+  table?: {
     row: PaletteColor;
+  };
+  pastelShades?: {
+    pink: PaletteColor;
+    lightPink: PaletteColor;
+    yellow: PaletteColor;
+    lightSalmon: PaletteColor;
+    lightGreen: PaletteColor;
   };
 }
 
