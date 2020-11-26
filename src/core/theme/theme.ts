9ebd14bec0ccb68428c4a0897761564fa9326f53
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-11-26 19:47:13 +0100

    updated theme and font added

--- src/core/theme/theme.ts
+++ src/core/theme/theme.ts
@@ -5,9 +5,16 @@ import { Theme } from './theme.vm';
 const defaultTheme = createMuiTheme();
 
 export const theme: Theme = merge(defaultTheme, {
+  typography: {
+    fontFamily: ['M PLUS Rounded 1c', 'sans-serif'].join(','),
+    // CUSTOM FONT FAMILY DOESN'T WORK
+  },
   palette: {
     background: {
       default: '#f9f9f9',
+    },
+    text: {
+      primary: '#305f72',
     },
     customPalette: {
       primaryLight: '#fff1ef',
@@ -19,14 +26,6 @@ export const theme: Theme = merge(defaultTheme, {
       lightYellow: '#fff5e5',
       lightBlue: '#eaf9fe',
       dark: '#305f72',
-    },
-  },
-  breakpoints: {
-    values: {
-      xs: 380,
-      sm: 578,
-      md: 728,
-      lg: 1100,
     },
   },
   spacing: (factor: number) => `${0.5 * factor}rem`, // 1 unit = 8px / 0.5rem
