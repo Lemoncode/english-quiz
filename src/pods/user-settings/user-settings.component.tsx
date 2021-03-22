3dfc8e6821397c18837b689eda13d8e3ed3698a0
Pedro Dominguez Aguilera <dominguezaguilera15@gmail.com>
2021-03-22 14:22:06 +0100

    Add styles

--- src/pods/user-settings/user-settings.component.tsx
+++ src/pods/user-settings/user-settings.component.tsx
@@ -9,7 +9,7 @@ import {
 import { SettingsEntity } from 'core/settings';
 import { QuestionsRestrictions } from 'core/const';
 import { Formik } from 'formik';
-
+import * as classes from './user-settings.styles';
 interface Props {
   onSave: (settings: SettingsEntity) => void;
   onCancel: () => void;
@@ -18,71 +18,88 @@ interface Props {
 
 export const UserSettingsComponent: React.FC<Props> = props => {
   const { onSave, onCancel, userSettings } = props;
+  const {
+    mainContainer,
+    title,
+    backContainer,
+    field,
+    buttonsContainer,
+  } = classes;
 
   //Material-UI limitation on TextField when prefilling the value
   const PREFILLED = true;
 
   return (
     <>
-      <Typography variant="h3">User settings:</Typography>
+      <main className={mainContainer}>
+        <h1 className={title}>User settings:</h1>
+        <div className={backContainer}>
+          <Formik
+            initialValues={{
+              numberQuestions: userSettings.numberQuestions,
+              secondChance: userSettings.secondChance,
+            }}
+            onSubmit={(values: SettingsEntity, { setSubmitting }) => {
+              setSubmitting(false);
+              onSave(values);
+            }}
+          >
+            {({ values, handleSubmit, isSubmitting, handleChange }) => (
+              <form onSubmit={handleSubmit}>
+                <TextField
+                  name="numberQuestions"
+                  label="Number of questions"
+                  type="number"
+                  InputLabelProps={{
+                    shrink: PREFILLED,
+                  }}
+                  variant="outlined"
+                  inputProps={{
+                    min: QuestionsRestrictions.MIN_NUMBER_QUESTIONS,
+                    max: QuestionsRestrictions.MAX_NUMBER_QUESTIONS,
+                    step: QuestionsRestrictions.STEP,
+                  }}
+                  value={values.numberQuestions}
+                  onChange={handleChange}
+                  className={field}
+                />
 
-      <Formik
-        initialValues={{
-          numberQuestions: userSettings.numberQuestions,
-          secondChance: userSettings.secondChance,
-        }}
-        onSubmit={(values: SettingsEntity, { setSubmitting }) => {
-          setSubmitting(false);
-          onSave(values);
-        }}
-      >
-        {({ values, handleSubmit, isSubmitting, handleChange }) => (
-          <form onSubmit={handleSubmit}>
-            <TextField
-              name="numberQuestions"
-              label="Number of questions"
-              type="number"
-              InputLabelProps={{
-                shrink: PREFILLED,
-              }}
-              variant="outlined"
-              inputProps={{
-                min: QuestionsRestrictions.MIN_NUMBER_QUESTIONS,
-                max: QuestionsRestrictions.MAX_NUMBER_QUESTIONS,
-                step: QuestionsRestrictions.STEP,
-              }}
-              value={values.numberQuestions}
-              onChange={handleChange}
-            />
-
-            <li>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    name="secondChance"
-                    color="primary"
-                    checked={values.secondChance}
-                    onChange={handleChange}
+                {/* <li>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        name="secondChance"
+                        color="primary"
+                        checked={values.secondChance}
+                        onChange={handleChange}
+                      />
+                    }
+                    label="Allow second chance"
                   />
-                }
-                label="Allow second chance"
-              />
-            </li>
-
-            <Button
-              variant="contained"
-              color="primary"
-              type="submit"
-              disabled={isSubmitting}
-            >
-              Save
-            </Button>
-            <Button variant="contained" color="secondary" onClick={onCancel}>
-              Cancel
-            </Button>
-          </form>
-        )}
-      </Formik>
+                </li>
+                  */}
+                <div className={buttonsContainer}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Save
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={onCancel}
+                  >
+                    Cancel
+                  </Button>
+                </div>
+              </form>
+            )}
+          </Formik>
+        </div>
+      </main>
     </>
   );
 };
