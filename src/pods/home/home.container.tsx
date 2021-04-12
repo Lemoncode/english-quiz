556f4157c5a07b5f138e9e091095534947f33eb3
Luis@newfile.es <luis.antonio.comi@gmail.com>
2021-04-12 23:34:33 +0200

    Menú principal

--- src/pods/home/home.container.tsx
+++ src/pods/home/home.container.tsx
@@ -1,23 +1,4 @@
 import * as React from 'react';
-import { HomeComponent } from './home.component';
-import { loadFullVerbCollection, loadSelectedVerbCollection } from 'core/verbs';
-import { globalVerbsContext } from 'core/verbs';
+import { HomeComponent } from '../home/home.component';
 
-export const HomeContainer = () => {
-  const verbsContext = React.useContext(globalVerbsContext);
-
-  const loadAllVerbs = async () => {
-    const fullVerbCollection = await loadFullVerbCollection();
-    verbsContext.setVerbCollection(fullVerbCollection);
-
-    const selectedVerbs: string[] = await loadSelectedVerbCollection();
-    verbsContext.setSelectedVerbs(selectedVerbs);
-  };
-
-  // Let's load the full list of verbs
-  React.useEffect(() => {
-    loadAllVerbs();
-  }, []);
-
-  return <HomeComponent />;
-};
+export const HomeContainer = () => <HomeComponent />;
---
+++ src/pods/home/home.styles.ts
@@ -0,0 +1,64 @@
+import { css } from 'emotion';
+import { theme } from '../../core/theme';
+
+const { palette, spacing, breakpoints } = theme;
+const color = palette.customPalette;
+
+export const mainContainer = css`
+  background-color: ${color.lightWhite};
+  width: 90%;
+  min-height: 100vh;
+  @media (min-width: ${breakpoints.values.xs}px) {
+    width: 70%;
+  }
+  @media (min-width: ${breakpoints.values.sm}px) {
+    max-width: 24rem;
+  }
+`;
+
+export const title = css`
+  margin-top: ${spacing(4)};
+  color: ${color.dark};
+  font-size: 1.6rem;
+  font-weight: 700;
+  text-align: center;
+  @media (min-width: ${breakpoints.values.xs}px) {
+    font-size: 1.75rem;
+  }
+`;
+
+export const backContainer = css`
+  margin-top: ${spacing(3)};
+  margin-bottom: ${spacing(0)};
+  padding: ${spacing(5)} ${spacing(5)};
+  background-color: ${color.lightWhite};
+  border-radius: 3rem;
+`;
+
+export const field = css`
+  width: 100%;
+  padding: 1rem;
+`;
+
+export const buttonsContainer = css`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  margin-top: 1rem;
+`;
+
+export const testFillGapButton = css`
+  background-color: #43e368;
+  color: ${color.lightWhite};
+  &:hover {
+    background-color: #31a64c;
+  }
+`;
+
+export const userSettingsButton = css`
+  background-color: #666666;
+  color: ${color.lightWhite};
+  &:hover {
+    background-color: #333333;
+  }
+`;
