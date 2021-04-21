a9e5d214d6336811648128fc91d1b9f865b79927
daniel.sanchez <daniel.sanchez@lemoncode.net>
2021-04-21 09:15:52 +0200

    move redire middleware before express static

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
-const {redirectHttpsMiddleware} = require('./redirect-https.middleware')
+const { redirectHttpsMiddleware } = require('./redirect-https.middleware');
 
 const app = express();
 
-const staticFilesPath = path.resolve(__dirname, process.env.STATIC_FILES_PATH);
-app.use('/', express.static(staticFilesPath));
-
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(redirectHttpsMiddleware);
 }
+
+const staticFilesPath = path.resolve(__dirname, '../dist');
+app.use('/', express.static(staticFilesPath));
 
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
--- server/redirect-https.middleware.js
+++ server/redirect-https.middleware.js
@@ -1,17 +1,17 @@
 const httpsProtocol = 'https';
-const protocol = 'x-forwarded-proto'
-const host = 'host'
+const protocol = 'x-forwarded-proto';
+const host = 'host';
 
-const buildHttpsUrl = (req) => `${httpsProtocol}://${req.header(host)}${req.url}`
+const buildHttpsUrl = req => `${httpsProtocol}://${req.header(host)}${req.url}`;
 
 const redirectHttpsMiddleware = (req, res, next) => {
-  if(req.header(protocol) !== httpsProtocol) {
+  if (req.header(protocol) !== httpsProtocol) {
     res.redirect(301, buildHttpsUrl(req));
   } else {
     next();
   }
-}
+};
 
 module.exports = {
-  redirectHttpsMiddleware
-}
+  redirectHttpsMiddleware,
+};
