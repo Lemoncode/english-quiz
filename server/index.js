a7b56ade6653e3a21077955adcaf7a888d97d50f
daniel.sanchez <daniel.sanchez@lemoncode.net>
2021-04-21 17:38:46 +0200

    remove redirect

--- server/index.js
+++ server/index.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const path = require('path');
-const { redirectHttpsMiddleware } = require('./redirect-https.middleware');
 
 const app = express();
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(redirectHttpsMiddleware);
-}
 
 const staticFilesPath = path.resolve(__dirname, process.env.STATIC_FILES_PATH);
 app.use('/', express.static(staticFilesPath));
