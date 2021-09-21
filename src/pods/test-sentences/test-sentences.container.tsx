df7710ea0aff5d3ff40604e3258566b45bc8860e
manudous <you@example.com>
2021-09-21 23:27:08 +0200

    add verbCollection to test-sentences.container

--- src/pods/test-sentences/test-sentences.container.tsx
+++ src/pods/test-sentences/test-sentences.container.tsx
@@ -3,20 +3,21 @@ import { loadFullSentencesCollection } from 'core/sentences';
 import { SentenceEntityApi } from 'core/sentences';
 import { pickRandomSentence } from './test-sentences.business';
 import { TestSentencesComponent } from './test-sentences.component';
-import { VerbEntityGlobal } from 'core/verbs';
 import { emptySentence, SentenceEntityVm } from './test.sentences.vm';
 import { mapFromSentenceApiToSentenceVm } from './test-sentences.mappers';
-
-export const verb: VerbEntityGlobal[] = [
-  {
-    infinitive: 'win',
-    past: 'won',
-    participle: 'won',
-    translation: 'ganar',
-  },
-];
+import { globalVerbsContext } from 'core/verbs';
 
 export const TestSentencesContainer: React.FC = () => {
+  const { verbCollection, selectedVerbs } = React.useContext(
+    globalVerbsContext
+  );
+
+  // I do not know if this filter to leave me only the selected 
+  // verbs is necessary or is it in another site already done
+  const selectedVerbsObject = verbCollection.filter(
+    x => selectedVerbs.indexOf(x.infinitive) > -1
+  );
+
   const [sentencesCollection, setSentencesCollection] = React.useState<
     SentenceEntityApi[]
   >([]);
@@ -35,15 +36,11 @@ export const TestSentencesContainer: React.FC = () => {
       const randomSentence = pickRandomSentence(sentencesCollection);
       const mapRandomSentence = mapFromSentenceApiToSentenceVm(
         randomSentence,
-        verb
+        selectedVerbsObject
       );
       setsentenceSelected(mapRandomSentence);
     }
   }, [sentencesCollection]);
 
-  return (
-    <TestSentencesComponent
-      sentenceSelected={sentenceSelected}
-    />
-  );
+  return <TestSentencesComponent sentenceSelected={sentenceSelected} />;
 };
