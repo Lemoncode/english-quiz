4ce927393b45cda8c092a3e2ef8c249736fb4753
felisuco069 <70918916+felisuco069@users.noreply.github.com>
2022-02-08 14:35:50 +0100

    Implementado el modal en el boton choose your verbs

--- src/pods/home/components/home-header.component.tsx
+++ src/pods/home/components/home-header.component.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
-import logo from '../../../assets/static/logo-english-quiz.png';
-import menuLogo from '../../../assets/static/menu.png';
+
 import * as classes from 'common/styles/tests.styles';
+import { ModalDialog } from './modal-dialog';
 
 export const HomeHeader = () => {
   const { headerHome, logoImage, menuImageHome, headerMenuHome } = classes;
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpenModal = () => {
+    setOpen(!open);
+  };
+
+  const body = (
+    <div>
+      <h2>Text in a modal</h2>
+      <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+    </div>
+  );
   return (
     <div className={headerHome}>
-      <img className={logoImage} src={logo} alt="imagen logo" />
-      <div className={headerMenuHome}>
+      <img
+        className={logoImage}
+        src={'assets/static/logo-english-quiz.png'}
+        alt="imagen logo"
+      />
+      <div className={headerMenuHome} onClick={handleOpenModal}>
         <span>Verbs settings</span>
-        <img className={menuImageHome} src={menuLogo} alt="imagen menu" />
+        <img
+          className={menuImageHome}
+          src={'assets/static/menu.png'}
+          alt="imagen menu"
+        />
+        {open ? <ModalDialog open={open} /> : null}
       </div>
     </div>
   );
---
+++ src/pods/home/components/modal-dialog.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import * as classes from 'common/styles/tests.styles';
+
+interface Props {
+  open: boolean;
+}
+
+const { modalDialogContent, modalDialogTitle } = classes;
+
+export const ModalDialog: React.FC<Props> = (props) => {
+  const { open } = props;
+
+  return (
+    <Dialog
+      PaperProps={{
+        style: { borderRadius: 20 },
+      }}
+      open={open}
+      aria-labelledby="dialog-title"
+      keepMounted
+    >
+      <DialogTitle className={modalDialogTitle} id="dialog-title">
+        {'Choose your verbs'}
+      </DialogTitle>
+      <DialogContent className={modalDialogContent}></DialogContent>
+    </Dialog>
+  );
+};
