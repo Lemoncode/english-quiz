9185db0e882559193731bdb869bf8a03f784c5cb
daniel.sanchez <daniel.sanchez@lemoncode.net>
2022-09-26 14:40:02 +0200

    update to bash

--- dispatch-update-route53.sh
+++ dispatch-update-route53.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
+#!/bin/bash
 curl -d "{\"event_type\": \"${EVENT}\"}" -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.everest-preview+json" "https://api.github.com/repos/${ORGANIZATION}/${REPOSITORY}/dispatches"
